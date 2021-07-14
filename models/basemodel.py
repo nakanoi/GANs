55a0a65fb1e880c5bb39ecefb9ee83a51ce05707
nakanoi <nakanoinryota@gmail.com>
2021-07-14 23:35:49 +0900

    Add Original GAN

--- models/basemodel.py
+++ models/basemodel.py
@@ -32,7 +32,7 @@ class BaseModel:
     def activation(self, act, alpha=0.2):
         act = act.lower()
         if act == 'leakrelu':
-            activation = LeakyReLU(alpha=0.2)
+            activation = LeakyReLU(alpha=alpha)
         elif act == 'relu':
             activation = ReLU()
         else:
@@ -51,22 +51,38 @@ class BaseModel:
             model.summary()
 
     def plot_models(self, folder='.'):
+        folder = os.path.join(folder, 'plots')
+        os.makedirs(folder, exist_ok=True)
+
         for name, model in self.models.items():
-            file_name = os.path.join(folder, 'plots/' + name + '.png')
+            file_name = os.path.join(folder, name + '.png')
             plot_model(model, to_file=file_name, show_shapes=True)
 
     def save_params(self, folder='.'):
+        os.makedirs(folder, exist_ok=True)
+
         file_name = os.path.join(folder, 'params.pkl')
         with open(file_name, 'wb') as f:
             pickle.dumps(*self.params, f)
 
-    def save_models(self, folder='.'):
+    def save_weights(self, folder='.', file_name='weights.ht'):
+        folder = os.path.join(folder, 'weights')
+        os.makedirs(folder, exist_ok=True)
+
         for name, model in self.models.items():
-            file_name = os.path.join(folder, 'models/' + name + '.png')
+            file = os.path.join(folder + name + file_name)
+            model.save_weights(file)
+
+    def save_models(self, folder='.'):
+        folder = os.path.join(folder, 'models')
+        os.makedirs(folder, exist_ok=True)
+
+        for name, model in self.models.items():
+            file_name = os.path.join(folder, name + '.png')
             model.save(file_name)
 
     def load_models(self, folder='.'):
         for name, model in self.models.imtes():
-            file_name = os.path.join(folder, 'models/' + name + '.png')
+            file_name = os.path.join(folder, 'models', name + '.png')
             model.load_weights(file_name)
 
