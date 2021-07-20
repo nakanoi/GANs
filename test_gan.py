3054834a400086bff77f972d293b5cbaeb431c5d
nakanoi <nakanoinryota@gmail.com>
2021-07-21 01:09:20 +0900

    add test(sample running) file

---
+++ test_gan.py
@@ -0,0 +1,28 @@
+from models import GAN 
+
+
+if __name__ == '__main__':
+    MODE =  'build'
+    BATCH_SIZE = 64
+    MAX_EPOCHS = 6000
+    SHOW_EVERY_N = 600
+
+    # Model
+    model = GAN(
+        data_name='mnist',
+        ID=0,
+        )
+    model.summary()
+    if MODE == 'load':
+        model.load_models()
+
+    # Load Data
+    (x_train, y_train), (x_test, y_test) = model.loader.load_np_data()
+
+    # Train
+    model.train(
+        BATCH_SIZE,
+        MAX_EPOCHS,
+        SHOW_EVERY_N,
+        train_data=[x_train, y_train]
+        )
---
+++ test_wgan.py
@@ -0,0 +1,27 @@
+from models import WGAN 
+
+
+if __name__ == '__main__':
+    MODE =  'build'
+    BATCH_SIZE = 64
+    MAX_EPOCHS = 6000
+    SHOW_EVERY_N = 600
+
+    # Model
+    model = WGAN(
+        data_name='mnist',
+        ID=0,
+        )
+    if MODE == 'load':
+        model.load_models()
+
+    # Load Data
+    (x_train, y_train), (x_test, y_test) = model.loader.load_np_data()
+
+    # Train
+    model.train(
+        BATCH_SIZE,
+        MAX_EPOCHS,
+        SHOW_EVERY_N,
+        train_data=[x_train, y_train]
+        )
